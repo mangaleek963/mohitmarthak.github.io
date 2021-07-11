9fc8201153299d0b3b5660fd21434bf9c8f8213f
mangaleek963 <87259153+mangaleek963@users.noreply.github.com>
2021-07-11 13:14:41 +0530

    resume

---
+++ js/app.js
@@ -0,0 +1,134 @@
+$(document).ready(function(){
+
+    $('#profile_ripple').ripples({
+        resolution: 512,
+        dropRadius: 10
+    });
+
+    const bars = document.querySelectorAll('.progress_bar');
+
+    bars.forEach(function(bar){
+        let percentage = bar.dataset.percent;
+        let tooltip = bar.children[0];
+        tooltip.innerText = percentage + '%';
+        bar.style.width = percentage + '%';
+    })
+
+    // Counter
+
+    const counters = document.querySelectorAll('.counter');
+    console.log(counters);
+    function runCounter(){
+        console.log("run function");
+        counters.forEach(counter => {
+            counter.innerText = 0;
+
+            let target = +counter.dataset.count;
+
+            let step = target / 100;     
+
+            let countIt = function() {
+                let displayedCount = +counter.innerText;
+                console.log(counter);
+                if(displayedCount < target){
+                    counter.innerText = Math.ceil(displayedCount + step);
+                    setTimeout(countIt, 1);
+                }else{
+                    counter.innerText = target;
+                }
+
+            }
+
+            countIt();
+
+        })
+    }
+    
+    
+
+
+    let counterSection = document.querySelector('.counter_section');
+
+    let options = {
+        rootMargin : '0px 0px -100px 0px',
+    }
+
+    let done = 0;
+
+    const  sectionObserver = new IntersectionObserver(function(entries){
+
+        if(entries[0].isIntersecting && done !=1){
+            done = 1;
+            runCounter();
+        }
+
+    },options)
+
+    sectionObserver.observe(counterSection);
+
+
+
+
+
+    // image filter
+
+    var $wrapper = $('.portfolio_wrapper');
+
+    // Initialize isotope
+
+    $wrapper.isotope({
+        filter : '*',
+        layoutMode : 'masonry',
+        animationOptions : {
+            duration : 750,
+            easing : 'liner',
+        }
+    });
+
+    let links = document.querySelectorAll('.tabs a');
+
+    links.forEach(link => {
+
+        let selector = link.dataset.filter;
+
+        link.addEventListener('click',function(e){
+            e.preventDefault();
+            $wrapper.isotope({
+                filter : selector,
+                layoutMode : 'masonry',
+                animationOptions : {
+                    duration : 750,
+                    easing : 'liner',
+                }
+            });
+
+            e.target.classList.add('active');
+            links.forEach(link =>{
+                link.classList.remove('active');
+            })
+
+            e.target.classList.add('active');
+
+        });
+    })
+
+    // magnify Pop up
+
+    $('.magnify').magnificPopup({
+        type: 'image',
+        gallery: {
+            enabled : true
+        },
+        zoom : {
+            enabled : true
+        }
+    });
+
+    //slider
+
+    $('.slider').slick({
+        arrows: false,
+        autoplay: true,
+    });
+
+});
